dd046a0ba31716785663045d2a24a01d743675cd
Ivan Fonin <mail@ivanfonin.com>
2017-06-27 18:18:14 +0300

    Fixed typo in admin scripts enqueue function. Enqueue script into the footer.

--- src/functions.php
+++ src/functions.php
@@ -126,7 +126,7 @@ add_filter( 'show_admin_bar', '__return_false' );
 function theme_admin_scripts() {
     wp_enqueue_script( 'app-manifest', get_template_directory_uri() . '/assets/js/manifest.js', '', '', true );
     wp_enqueue_script( 'app-vendor', get_template_directory_uri() . '/assets/js/vendor.js', '', '', true );
-    wp_enqueue_script( 'admin-scripts', get_template_directory_uri() . '/assets/js/admin/admin.js' );
+    wp_enqueue_script( 'admin-scripts', get_template_directory_uri() . '/assets/js/admin/admin.js', '', '', true );
 }
 add_action( 'admin_enqueue_scripts', 'theme_admin_scripts' );
 
