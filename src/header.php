4b564ddd53c22ec12fba533d0aa785e40af32ccd
Ivan Fonin <mail@ivanfonin.com>
2017-07-04 12:48:00 +0300

    Removed browsersync task from package.json and added some custom styles to play around with UIkit.

--- src/header.php
+++ src/header.php
@@ -16,15 +16,11 @@
 </head>
 <body <?php body_class(); ?>>
     <div id="app">
-        <header class="uk-navbar-container tm-navbar-container uk-sticky uk-sticky-fixed">
-            <div class="uk-container uk-container-expand">
-                <nav class="uk-navbar">
-                    <div class="uk-navbar-left">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="uk-navbar-item uk-logo">
-                            <?php bloginfo( 'name' ); ?>
-                        </a>
-                    </div>
-                    <?php wp_nav_menu( array( 'theme_location' => 'main', 'container_class' => 'uk-navbar-right', 'menu_class' => 'uk-navbar-nav' ) ); ?>
-                </nav>
+        <nav class="uk-navbar uk-navbar-container uk-sticky uk-sticky-fixed webm-navbar-container" uk-navbar>
+            <div class="uk-navbar-left">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="uk-navbar-item uk-logo">
+                    <?php bloginfo( 'name' ); ?>
+                </a>
             </div>
-        </header>
+            <?php wp_nav_menu( array( 'theme_location' => 'main', 'container_class' => 'uk-navbar-right', 'menu_class' => 'uk-navbar-nav' ) ); ?>
+        </nav>
