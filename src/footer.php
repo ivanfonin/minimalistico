f55e203a02cdcc6b193c3763cc96167f67054dad
Ivan Fonin <mail@ivanfonin.com>
2017-07-07 13:14:55 +0300

    Added PT Sans fonts, configured font-family variables. Added locate-template function inside page-templates/home.php, because get_template_part is not working from inside subfolder. Added footer section with call to action.

--- src/footer.php
+++ src/footer.php
@@ -4,12 +4,23 @@
  */
 ?>
 
-        <footer class="uk-section uk-section-secondary uk-section-large uk-padding-remove-bottom">
+        <div class="uk-section uk-section-muted uk-section-large">
+            <div class="uk-container">
+                <div class="uk-flex-middle uk-text-center">
+                    <h2>Хотите работать с нами?</h2>
+                    <p>Оставьте заявку на сайте и мы свяжемся с Вами в течение рабочего дня.</p>
+                    <a href="#" class="uk-button uk-button-text">Оставить заявку</a>
+                </div>
+            </div>
+        </div>
+
+        <footer class="uk-section uk-section-default uk-padding-remove-bottom uk-padding-remove-top">
             <div class="uk-container">
                 <div class="uk-margin-top uk-margin-bottom">
                     <div class="uk-child-width-auto@m uk-flex-middle" uk-grid>
                         <div class="uk-width-1-4@m uk-text-left@m uk-text-center uk-text-small" uk-scrollspy="cls:uk-animation-slide-left-small">
-                            <p>&copy; 2014-<?php echo date('Y'); ?>, <a href="#">«Веб Минимализм»</a></p>
+                            <p>
+                                &copy; 2014-<?php echo date('Y'); ?>, <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">«<?php bloginfo( 'name' ); ?>»</a></p>
                         </div>
                         <div class="uk-width-1-2@m" uk-scrollspy="cls:uk-animation-slide-bottom-small">
                             <?php if ( has_nav_menu( 'main' ) ) : ?>
--- src/page-templates/home.php
+++ src/page-templates/home.php
@@ -11,7 +11,7 @@ __( 'Home Page', 'themestarter' ); ?>
     <main class="uk-container uk-section">
         <?php while ( have_posts() ) : the_post(); ?>
 
-            <?php get_template_part( 'content', 'page' ); ?>
+            <?php locate_template( 'partials/content/page.php', true, true ); ?>
 
             <?php if ( comments_open() || get_comments_number() ) : ?>
                 <?php comments_template(); ?>
