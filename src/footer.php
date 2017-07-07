79bbb908bcb23b320b887f0bcf2fed21f9134270
Ivan Fonin <mail@ivanfonin.com>
2017-07-07 13:44:26 +0300

    Added uk-button-text underline animation on hover with mixin. Prepared custom navbar.scss and navbar-mixins.scss to style navbar.

--- src/footer.php
+++ src/footer.php
@@ -7,9 +7,9 @@
         <div class="uk-section uk-section-muted uk-section-large">
             <div class="uk-container">
                 <div class="uk-flex-middle uk-text-center">
-                    <h2>Хотите работать с нами?</h2>
-                    <p>Оставьте заявку на сайте и мы свяжемся с Вами в течение рабочего дня.</p>
-                    <a href="#" class="uk-button uk-button-text">Оставить заявку</a>
+                    <h2 uk-scrollspy="cls:uk-animation-slide-top-small">Хотите работать с нами?</h2>
+                    <p uk-scrollspy="cls:uk-animation-slide-bottom-small">Оставьте заявку на сайте и мы свяжемся с Вами в течение рабочего дня.</p>
+                    <a href="#" class="uk-button uk-button-text" uk-scrollspy="cls:uk-animation-slide-bottom-small">Оставить заявку</a>
                 </div>
             </div>
         </div>
@@ -18,9 +18,8 @@
             <div class="uk-container">
                 <div class="uk-margin-top uk-margin-bottom">
                     <div class="uk-child-width-auto@m uk-flex-middle" uk-grid>
-                        <div class="uk-width-1-4@m uk-text-left@m uk-text-center uk-text-small" uk-scrollspy="cls:uk-animation-slide-left-small">
-                            <p>
-                                &copy; 2014-<?php echo date('Y'); ?>, <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">«<?php bloginfo( 'name' ); ?>»</a></p>
+                        <div class="uk-width-1-4@m uk-text-left@m uk-text-center" uk-scrollspy="cls:uk-animation-slide-left-small">
+                            <span class="uk-text-small">&copy; 2014-<?php echo date('Y'); ?>,</span> <a class="uk-text-small" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">«<?php bloginfo( 'name' ); ?>»</a>
                         </div>
                         <div class="uk-width-1-2@m" uk-scrollspy="cls:uk-animation-slide-bottom-small">
                             <?php if ( has_nav_menu( 'main' ) ) : ?>
@@ -32,8 +31,8 @@
                                 ); ?>
                             <?php endif; ?>
                         </div>
-                        <div class="uk-width-1-4@m uk-text-right@m uk-text-center uk-text-small" uk-scrollspy="cls:uk-animation-slide-right-small">
-                            <p><a href="#">Политика конфиденциальности</a></p>
+                        <div class="uk-text-center uk-width-1-4@m uk-text-right@m" uk-scrollspy="cls:uk-animation-slide-right-small">
+                            <a class="uk-button uk-inline uk-button-text uk-text-small" href="#">Политика конфиденциальности</a>
                         </div>
                     </div>
                 </div>
