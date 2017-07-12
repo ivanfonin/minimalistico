c11b579e392594f9c3d979fcd6f84b7b6f8ab914
Ivan Fonin <mail@ivanfonin.com>
2017-07-12 17:43:09 +0300

    Added offcanvas variables for the theme and fixed typo in call-to-action widget.

--- src/partials/widgets/call-to-action.php
+++ src/partials/widgets/call-to-action.php
@@ -1,7 +1,7 @@
 <div class="uk-section uk-section-muted uk-section-large">
     <div class="uk-container">
         <div class="uk-flex-middle uk-text-center">
-            <h2 uk-scrollspy="cls:uk-animation-slide-top-small"><?php _e('Хотите работать с нами?', 'themetester');</h2>
+            <h2 uk-scrollspy="cls:uk-animation-slide-top-small"><?php _e( 'Хотите работать с нами?', 'themetester' ); ?></h2>
             <p uk-scrollspy="cls:uk-animation-slide-bottom-small">Оставьте заявку на сайте и мы свяжемся с Вами в течение рабочего дня.</p>
             <a href="#" class="uk-button uk-button-text" uk-scrollspy="cls:uk-animation-slide-bottom-small">
                 <span class="uk-icon" uk-icon="arrow-right"></span>
