53a2e0ea8bed70a43b548a124287fd3e0e0958b1
Ivan Fonin <mail@ivanfonin.com>
2017-08-30 20:10:10 +0300

    Changed markup to center content on not-fount page.

--- src/index.php
+++ src/index.php
@@ -5,36 +5,35 @@
 
 get_header(); ?>
 
-<main role="main" uk-height-viewport="expand: true">
-
     <?php if ( have_posts() ) : ?>
 
-        <div class="uk-container uk-section-small">
-            <div class="uk-grid">
-                <div class="uk-width-3-4@m">
-                    <div class="uk-grid-medium uk-child-width-expand@s" uk-grid uk-height-match="target: > article; row: false">
-                        <?php while ( have_posts() ) : the_post(); ?>
-                            <?php get_template_part( 'partials/content/content', get_post_format() ); ?>
-                        <?php endwhile; ?>
+        <main role="main" uk-height-viewport="expand: true">
+            <div class="uk-container uk-section-small">
+                <div class="uk-grid">
+                    <div class="uk-width-3-4@m">
+                        <div class="uk-grid-medium uk-child-width-expand@s" uk-grid uk-height-match="target: > article; row: false">
+                            <?php while ( have_posts() ) : the_post(); ?>
+                                <?php get_template_part( 'partials/content/content', get_post_format() ); ?>
+                            <?php endwhile; ?>
+                        </div>
                     </div>
-                </div>
-                <div class="uk-width-1-4@m">
-                    <div class="uk-grid-medium">
-                        <?php get_sidebar(); ?>
+                    <div class="uk-width-1-4@m">
+                        <div class="uk-grid-medium">
+                            <?php get_sidebar(); ?>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
 
         <?php theme_print_posts_pagination(); ?>
 
     <?php else : ?>
 
-        <?php get_template_part( 'partials/content/none' ); ?>
+        <main role="main" class="uk-flex uk-flex-center uk-flex-middle" uk-height-viewport="expand: true">
+            <?php get_template_part( 'partials/content/none' ); ?>
+        </main>
 
     <?php endif; ?>
-
-</main>
-
 
 <?php get_footer(); ?>
