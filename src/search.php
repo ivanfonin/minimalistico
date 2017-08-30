316b4f0040f511b5d2d4d68bf104adab71295f02
Ivan Fonin <mail@ivanfonin.com>
2017-08-30 20:20:00 +0300

    Changed markup on search.php according to index.php and archive.php

--- src/search.php
+++ src/search.php
@@ -6,38 +6,34 @@
 
 <?php get_header(); ?>
 
-<main role="main">
+<?php if ( have_posts() ) : ?>
 
-    <?php if ( have_posts() ) : ?>
-
-        <div uk-height-viewport="expand: true">
-            <div class="uk-section uk-section-muted uk-section-xsmall">
-                <header class="uk-container">
-                    <h1 class="uk-text-uppercase uk-text-meta">
-                        <?php printf( __( '<span uk-icon="icon: search"></span> %s', 'minimalistico' ), get_search_query() ); ?>
-                    </h1>
-                </header>
-            </div>
-
-            <div class="uk-section-small">
-                <div class="uk-container">
-                    <?php while ( have_posts() ) : the_post(); ?>
-                        <?php get_template_part( 'partials/content/search' ); ?>
-                    <?php endwhile; ?>
-                </div>
-            </div>
-
-            <?php theme_print_posts_pagination(); ?>
+    <main role="main" uk-height-viewport="expand: true">
+        <div class="uk-section uk-section-muted uk-section-xsmall">
+            <header class="uk-container">
+                <h1 class="uk-text-uppercase uk-text-meta">
+                    <?php printf( __( '<span uk-icon="icon: search"></span> %s', 'minimalistico' ), get_search_query() ); ?>
+                </h1>
+            </header>
         </div>
 
-    <?php else : ?>
-
-        <div class="uk-flex uk-flex-center uk-flex-middle" uk-height-viewport="expand: true">
-            <?php get_template_part( 'partials/content/none' ); ?>
+        <div class="uk-section-small">
+            <div class="uk-container">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php get_template_part( 'partials/content/search' ); ?>
+                <?php endwhile; ?>
+            </div>
         </div>
 
-    <?php endif; ?>
+        <?php theme_print_posts_pagination(); ?>
+    </main>
 
-</main>
+<?php else : ?>
+
+    <main role="main" class="uk-flex uk-flex-center uk-flex-middle" uk-height-viewport="expand: true">
+        <?php get_template_part( 'partials/content/none' ); ?>
+    </main>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
