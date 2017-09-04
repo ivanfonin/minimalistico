d456373b9561a9438c01abf6cfe5dfde3d9f32d7
Ivan Fonin <mail@ivanfonin.com>
2017-09-04 11:30:26 +0300

    Removed page-sudebar login from sidebar.php file.

--- src/sidebar.php
+++ src/sidebar.php
@@ -9,9 +9,3 @@
         <?php theme_print_blog_sidebar(); ?>
     </aside>
 <?php endif; ?>
-
-<?php if ( is_active_sidebar( 'page-sidebar' ) ) : ?>
-    <aside class="sidebar" role="complementary">
-        <?php theme_print_sidebar_with_uikit_classes( 'page-sidebar' ); ?>
-    </aside>
-<?php endif; ?>
